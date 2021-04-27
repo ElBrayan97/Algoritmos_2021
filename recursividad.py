f084f84ce12996f0790da430098c2dafd4e10569
ElBrayan97 <49415822+ElBrayan97@users.noreply.github.com>
2021-04-27 19:04:40 -0300

    laberinto2

--- recursividad.py
+++ recursividad.py
@@ -114,18 +114,34 @@ def Usarlafuerza(lista, O_buscado, indice):
 
 
 
-def laberinto(matriz):
-    if laberinto()
+def laberinto(x, y, matriz):
+    if(x >= 0 and x <= len(matriz)-1) and (y >= 0 and y <= len(matriz[0])-1):
+        if (matriz[x][y] == 2):
+            print("Se Salio del Laberinto")
+        elif (matriz[x][y] == 1):
+            matriz[x][y] = 3
+            laberinto(x, y+1, matriz)
+            laberinto(x, y-1, matriz)
+            laberinto(x+1, y, matriz)
+            laberinto(x-1, y, matriz)
+            matriz[x][y] = 1
+    else:
 
-#print (romano("XCVII"))
+
+print (romano("CXVII"))
 
 mochila = ['objeto1','objeto2','objeto3','sable de luz','objeto4','objeto5']
 Usarlafuerza(mochila,'objeto5',0)
-#print (invertDecaBin(5))
+print (invertDecaBin(5))
 
-matriz =[ [0,0,1,1,1]
-          [1,0,0,0,1]
-          [1,1,0,1,1]
-          [1,0,0,1,1]
-          [1,0,0,0,2] ]
+lab = [[1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 1, 0, 0],
+       [1, 1, 1, 0, 1, 0, 1],
+       [1, 0, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 2]]
+
+laberinto(0, 0, lab)
+
+
 
