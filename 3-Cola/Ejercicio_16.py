6c6046b2c47b6e47ad3a4d8e19ab9585bc46c72b
ElBrayan97 <49415822+ElBrayan97@users.noreply.github.com>
2021-07-31 20:59:52 -0300

    Update Ejercicio_16.py
    terminado el ejercicio 16... una papa :P

--- 3-Cola/Ejercicio_16.py
+++ 3-Cola/Ejercicio_16.py
@@ -2,16 +2,59 @@ from TDA_Cola import Cola
 
 C = Cola()
 
-def cargar_cola(C):
-    C.arribo(["Steve Rogers","Capitán América","M"])
-    C.arribo(["Tony Stark","Iron Man", "M"])
-    C.arribo(["Natasha Romanoff","Black Widow","F"])
-    C.arribo(["Carol Danvers", "Capitana Marvel", "F"])
-    C.arribo(["Peter Parker", "Spyder-Man", "M"])
-    C.arribo(["Scot Lang", "Ant-Man", "M"])
-    C.arribo(["Stephen Strange", "Dr. Strange", "M"])
-    C.arribo(["Wanda Maximof", "Bruja Escarlata", "F"])
-    C.arribo(["Bruce Banner", "Hulk", "M"])
-    C.arribo(["T'Challa", "Pantera Negra", "M"])
-    print("Cola Cargada!")
+def cargarDocumento(C):
+    A=["",0]
+    A[0] = str(input("ingrese el nombre del documento: "))
+    A[1] = int(input("Ingrese 1: Empleados, 2: Staff 3: Gerente   :"))
+    C.arribo(A)
 
+
+def ordenarPrioridad(cola):
+    empleados = Cola()
+    staff = Cola()
+    gerente = Cola()
+
+    while not cola.cola_vacia():
+        A = cola.atencion()
+        if A[1]==1:
+            empleados.arribo(A)
+        elif A[1]==2:
+            staff.arribo(A)
+        elif A[1]==3:
+            gerente.arribo(A)
+
+    while (not gerente.cola_vacia()):
+        cola.arribo(gerente.atencion())
+
+    while (not staff.cola_vacia()):
+        cola.arribo(staff.atencion())
+
+    while (not empleados.cola_vacia()):
+        cola.arribo(empleados.atencion())
+
+def imprimirDocumento(cola):
+    print (cola.atencion()[0])
+
+
+cargarDocumento(C) #Empleados
+cargarDocumento(C) #Empleados
+cargarDocumento(C) #Empleados 
+
+cargarDocumento(C) #Staff
+cargarDocumento(C) #Staff
+
+cargarDocumento(C) #Gerente
+
+ordenarPrioridad(C) #Se ordena la cola por prioridad
+
+imprimirDocumento(C) #Se imprime el primer documento
+imprimirDocumento(C) #Se imprime el segundo documento
+
+cargarDocumento(C) #Se carga un documento de empleados
+cargarDocumento(C) #Se carga un documento de empleados
+cargarDocumento(C) #Se carga un documento de gerente
+
+ordenarPrioridad(C) #Se ordena la cola por prioridad
+
+while not C.cola_vacia(): # Se imprime toda la cola
+    imprimirDocumento(C)
